940b2643b41612e788576be5b86f4ba396ceccc2
Augustus Greenwood <gusbgreenwood@gmail.com>
2025-03-12 21:19:11 -0500

    added better documentation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # analysis
-# analysis
+
+Some functions to help analyzing data in octave. 
+
+To load the functions and make them available you need to 'addpath'
+
+So if I want to add all functions in 'fitting', i would run:
+
+addpath("/path/to/analysis/directory/fitting")
+
+and then all fitting functions would be available
--- fitting/linear_fit.m
+++ fitting/linear_fit.m
@@ -1,3 +1,8 @@
+% Takes x and y data points, fits a curve to them and returns 
+% the two parameters (slope and intercept),coefficient of determination
+% and the y values for the fit (to make plotting easier)
+% It is advisable to flatten your data, I'll do it for you to make things
+% easy, and it may not land how you would like  
 function [m, b, R2, yfit] = linear_fit(x, y)
     coeff = polyfit(x(:), y(:), 1);
     m = coeff(1);
